36d667ec8a9fc7529586cdbee22d3cae24b90454
hieuvau198 <nhatduongfpt2000@gmail.com>
2025-10-17 22:44:42 +0700

    fix/simulation-v6-traine-practice-step-attempt

--- Lssctc/Lssctc.SimulationManagement/TraineePractices/Services/ITraineeStepService.cs
+++ Lssctc/Lssctc.SimulationManagement/TraineePractices/Services/ITraineeStepService.cs
@@ -6,6 +6,8 @@ namespace Lssctc.SimulationManagement.TraineePractices.Services
     {
         // get steps by practice id and trainee id
         Task<List<TraineeStepDto>> GetTraineeStepsByPracticeIdAndTraineeId(int practiceId, int traineeId);
+        // get steps by attempt id
+        Task<List<TraineeStepDto>> GetTraineeStepsByAttemptId(int attemptId);
         // get step by step id and trainee id
         Task<TraineeStepDto?> GetTraineeStepByIdAndTraineeId(int stepId, int traineeId);
         // submit trainee step attempt
