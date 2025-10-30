ab36cac07d4345ad143df8dbd0e324c6dede2cab
hieuvau198 <nhatduongfpt2000@gmail.com>
2025-10-30 17:30:28 +0700

    doc/script-change

---
+++ lssctc_db-script-v6.sql
@@ -0,0 +1,74 @@
+================================================== New Database
+
+Script structure
+
+# Account area
+user
+admin
+simulation manager
+instructor
+trainee
+instructor profile
+trainee profile
+
+# Program & Course area
+program
+course code
+course category
+course level
+course
+program course
+
+# Section area
+section
+course section
+
+# Activity area
+activity
+section activity
+
+# Material area
+learning material
+activity material
+
+# Quiz area
+quiz
+quiz question
+question option
+activity quiz
+
+# Practice area
+practice
+task
+practice task
+activity practice
+brand model
+sim component
+
+=======
+
+# class area
+class code
+program course class
+class instructor
+enrollment
+
+# progress area
+learning progress
+section record
+activity record
+
+# quiz attempt
+quia attempt
+quiz attempt question
+quiz attempt answer
+
+# practice attempt
+practice attempt
+practice attempt task
+
+# certificate area
+certificate
+course certificate
+trainee certificate
+
