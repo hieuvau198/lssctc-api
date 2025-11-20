14d2521ef938fb987cb29ec6a8096bfe73cef443
hieuvau198 <nhatduongfpt2000@gmail.com>
2025-11-20 19:51:18 +0700

    fix/db-script

--- special_script_syntax.sql
+++ special_script_syntax.sql
@@ -44,3 +44,31 @@ ADD [section_order] INT NOT NULL DEFAULT 0;
 ALTER TABLE [dbo].[section_records]
 ADD [duration_minutes] INT NULL 
     CONSTRAINT DF_section_records_duration_minutes DEFAULT 20;
+
+
+alter table practices
+add [practice_code] nvarchar null default 'NO PRACTICE CODE ASSIGNED'
+
+ALTER TABLE practices
+ALTER COLUMN practice_code VARCHAR(50);
+
+SELECT COLUMN_NAME, DATA_TYPE, CHARACTER_MAXIMUM_LENGTH
+FROM INFORMATION_SCHEMA.COLUMNS
+WHERE TABLE_NAME = 'practices'
+  AND COLUMN_NAME = 'practice_code';
+
+  ALTER TABLE practices
+DROP CONSTRAINT DF__practices__pract__03FB8544;
+
+ALTER TABLE practices
+ADD CONSTRAINT DF_practices_practice_code
+DEFAULT('NO PRACTICE CODE ASSIGNED') FOR practice_code;
+
+ALTER TABLE sim_tasks
+ADD task_code NVARCHAR(50) default 'NO PRACTICE CODE ASSIGNED'
+
+update sim_tasks
+set task_code = 'TASK_UNKNOWN'
+
+alter table simulation_components
+add component_code nvarchar(50) NULL default 'COMPONENT_UNKNOWN'
